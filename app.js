de53e9fc3a7489ca38d17d8df59ecb59e59d6cf5
unknown <nomgnenoel@gmail.com>
2023-10-15 03:18:16 +0100

    update

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hello, NodeJS !');
+const express = require('express');
+
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello Express 2 !');
+});
+app.listen(
+    port, 
+    () => console.log('Notre application node est demarée sur : http://localhost:${port}')
+);
